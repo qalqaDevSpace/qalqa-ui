5c5ece37fc7df2d615e578241f3526f1d5fc56eb
qalqaa <113594321+qalqaa@users.noreply.github.com>
2025-01-06 18:54:18 +0300

    dev: added pnpm to .gitignore
    dev: added pnpm to .gitignore

---
+++ src/App/App.tsx
@@ -0,0 +1,30 @@
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1>Test Stand for component</h1>
+        <h2>BaseStyles of elements</h2>
+        <h3>Title 3</h3>
+        <h4>Title 4</h4>
+        <h5>Title 5</h5>
+        <h6>Title 6</h6>
+        <p>Paragraph</p>
+        <a href="#">Link</a>
+        <button>Button</button>
+        <input type="text" placeholder="Input" />
+        <div>Div</div>
+        <span>Span</span>
+        <ul>
+          <li>Unordered List item 1</li>
+          <li>Unordered List item 2</li>
+        </ul>
+        <ol>
+          <li>Ordered List item 1</li>
+          <li>Ordered List item 2</li>
+        </ol>
+      </div>
+    </>
+  );
+}
+
+export default App;
