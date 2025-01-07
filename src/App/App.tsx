38f190cc00d3ca586f19f3c4a850160e21fbad07
qalqaa <113594321+qalqaa@users.noreply.github.com>
2025-01-07 21:46:06 +0300

    dev(stand): some QoL features

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -5,26 +5,6 @@ function App() {
   return (
     <>
       <div className={styles.container}>
-        <h1>Test Stand for component</h1>
-        <h2>BaseStyles of elements</h2>
-        <h3>Title 3</h3>
-        <h4>Title 4</h4>
-        <h5>Title 5</h5>
-        <h6>Title 6</h6>
-        <p>Paragraph</p>
-        <a href="#">Link</a>
-        <button>Button</button>
-        <input type="text" placeholder="Input" />
-        <div>Div</div>
-        <span>Span</span>
-        <ul>
-          <li>Unordered List item 1</li>
-          <li>Unordered List item 2</li>
-        </ul>
-        <ol>
-          <li>Ordered List item 1</li>
-          <li>Ordered List item 2</li>
-        </ol>
         <Button label="Click Me"></Button>
       </div>
     </>
