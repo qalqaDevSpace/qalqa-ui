a6fa9b6f3ab4cdff43dd4353dddab8580405b60a
qalqaa <113594321+qalqaa@users.noreply.github.com>
2025-01-08 04:22:48 +0300

    feat: added ProgressBar component

---
+++ src/components-lib/components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import { ToastType } from "../../model/ToastModel";
+import styles from "./ProgressBar.module.scss";
+import clsx from "clsx";
+
+type ProgressBarProps = {
+  duration: number;
+  type: ToastType;
+};
+
+const ProgressBar: React.FC<ProgressBarProps> = ({
+  duration,
+  type,
+}: ProgressBarProps) => {
+  const [progress, setProgress] = useState(100);
+
+  useEffect(() => {
+    let interval: ReturnType<typeof setInterval>;
+    setTimeout(() => {
+      if (duration > 0) {
+        const step = 100 / (duration * 10);
+        interval = setInterval(() => {
+          setProgress((prev) => {
+            if (prev <= 0) {
+              clearInterval(interval);
+              return 0;
+            }
+            return prev - step;
+          });
+        }, 100);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [duration]);
+
+  return (
+    <div className={styles.progressbar}>
+      <div
+        style={{ width: `${progress}%` }}
+        className={clsx(styles.progress, styles[`t-${type}`])}
+      />
+    </div>
+  );
+};
+
+export default ProgressBar;
