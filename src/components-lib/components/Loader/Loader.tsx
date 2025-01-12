3837c1ee06d11c3ee63bede9b1de1687668cb0a6
TakhirovBizhan <149726252+TakhirovBizhan@users.noreply.github.com>
2025-01-13 02:38:57 +0300

    fixed: fixed loading model. Added interfaces, types for speed, stroke and opacity.

--- src/components-lib/components/Loader/Loader.tsx
+++ src/components-lib/components/Loader/Loader.tsx
@@ -1,12 +1,23 @@
-import { ILoaderProps } from "../../model/LoaderModel";
+import clsx from "clsx";
+import { LoaderProps } from "../../model/LoaderModel";
+import s from './Loader.module.scss'
 
- const Loader: React.FC<ILoaderProps> = ({
-    loaderType = 'ring',
+ const Loader: React.FC<LoaderProps> = ({
+    type = 'ring',
     loaderSize = 'md',
     className = '',
+    speed = 2,
+    BGOpacity = 0,
+    stroke = 6,
 }) => {
 
-    const loaderClassNames = 
+    const loaderClassNames =  clsx(
+      s.loader,
+      s[`t-${type}`],
+      s[`s-${loaderSize}`],
+      className,
+    );
+
   return (
     <div>Loader</div>
   )
