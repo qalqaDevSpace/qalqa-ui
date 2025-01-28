cff4049ef01ea6b4e23d227eda52b04d1d0c2fd3
TakhirovBizhan <149726252+TakhirovBizhan@users.noreply.github.com>
2025-01-28 20:46:08 +0300

    feat: dots  markdown is done. scss styles are done for dots and ring.

--- src/components-lib/components/Loader/Loader.tsx
+++ src/components-lib/components/Loader/Loader.tsx
@@ -4,28 +4,46 @@ import s from "./Loader.module.scss";
 
 const Loader: React.FC<LoaderProps> = ({
   type = "ring",
-  loaderSize = "md",
+  loaderSize = "md", //done
   className = "",
   speed = "default",
-  BGOpacity = "none",
-  stroke = "default",
+  BGOpacity = "none", //done
+  stroke = "default", //done
 }) => {
-  const loaderClassNames = clsx(
-    s.container,
-    s.container_ring,
-    s.loader,
-    s[`size-${loaderSize}`],
-    s[`speed-${speed}`],
-    className
+  //base
+  const loaderClassnames = clsx(s.loader, s[`container_${type}`], className);
+  // ring
+  const ringLoaderClassNames = clsx(
+    loaderClassnames,
+    s[`r-speed-${speed}`],
+    s[`r-size-${loaderSize}`]
   );
 
   const trackClassnames = clsx(s.track, s[`opacity-${BGOpacity}`]);
+  const carClassnames = clsx(s.car, s[`car-speed-${speed}`]);
+
+  //dots
+  const dotsLoaderClassnames = clsx(
+    loaderClassnames,
+    s[`d-size-${loaderSize}`]
+  );
+
+  const dotsCubeClassnames = clsx(
+    s.cube,
+    s[`cube-size-${loaderSize}`],
+    s[`cube-speed-${speed}`]
+  );
+
+  const dotsInnerCubeClassnames = clsx(
+    s.cube__inner,
+    s[`cube__inner-speed-${speed}`]
+  );
 
   return (
     <>
       {type == "ring" && (
         <svg
-          className={loaderClassNames}
+          className={ringLoaderClassNames}
           viewBox="0 0 40 40"
           height="40"
           width="40"
@@ -40,7 +58,7 @@ const Loader: React.FC<LoaderProps> = ({
             fill="none"
           />
           <circle
-            className={s.car}
+            className={carClassnames}
             cx="20"
             cy="20"
             r="17.5"
@@ -49,6 +67,20 @@ const Loader: React.FC<LoaderProps> = ({
             fill="none"
           />
         </svg>
+      )}
+
+      {type == "dots" && (
+        <div className={dotsLoaderClassnames}>
+          <div className={dotsCubeClassnames}>
+            <div className={dotsInnerCubeClassnames}></div>
+          </div>
+          <div className={dotsCubeClassnames}>
+            <div className={dotsInnerCubeClassnames}></div>
+          </div>
+          <div className={dotsCubeClassnames}>
+            <div className={dotsInnerCubeClassnames}></div>
+          </div>
+        </div>
       )}
     </>
   );
