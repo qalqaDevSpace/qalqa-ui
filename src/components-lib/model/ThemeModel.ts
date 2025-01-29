171fc832676f3e04964ce23b340a749c8a3de730
qalqa <andreybas04@gmail.com>
2025-01-29 16:11:34 +0300

    big:feat: completed Theme Provoder + also added 2 default thems
    it actually need big doc

---
+++ src/components-lib/model/ThemeModel.ts
@@ -0,0 +1,43 @@
+export type Theme = ThemeEnum.DARK | ThemeEnum.LIGHT; //TODO: В дальнейшем нужно будет предусмотреть, что тут могут быть произвольные пользовательские темы, меняемые по ключу.
+
+export enum ThemeEnum {
+	DARK = 'dark',
+	LIGHT = 'light',
+}
+
+export interface ThemeContextProps {
+	theme: Theme | string;
+	toggleTheme: (theme: Theme) => void;
+}
+
+interface ThemeProviderBaseProps {
+	children: React.ReactNode;
+}
+
+export type ThemeProviderPropsWithTheming = ThemeProviderBaseProps & {
+	isTheming: true;
+	isCustomTheme?: boolean;
+};
+
+export type ThemeProviderPropsWithoutTheming = ThemeProviderBaseProps & {
+	isTheming?: false;
+	isCustomTheme?: never;
+};
+
+interface ThemeProviderPropsWithCustomTheme extends ThemeProviderBaseProps {
+	isCustomTheme?: true;
+	customThemeNameDark: string;
+	customThemeNameLight: string;
+}
+
+interface ThemeProviderPropsWithoutCustomTheme extends ThemeProviderBaseProps {
+	isCustomTheme?: false;
+	customThemeNameDark?: never;
+	customThemeNameLight?: never;
+}
+
+export type ThemeProviderProps = (
+	| ThemeProviderPropsWithTheming
+	| ThemeProviderPropsWithoutTheming
+) &
+	(ThemeProviderPropsWithCustomTheme | ThemeProviderPropsWithoutCustomTheme);
