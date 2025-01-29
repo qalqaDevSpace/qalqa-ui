16e385d110d2c481468268f01a511352fc609572
qalqa <andreybas04@gmail.com>
2025-01-29 17:37:19 +0300

    feat: added some props for themeButton

--- src/components-lib/components/ThemeButton/ThemeButton.tsx
+++ src/components-lib/components/ThemeButton/ThemeButton.tsx
@@ -1,11 +1,18 @@
+import clsx from 'clsx';
+import { ThemeButtonProps } from '../../model/ThemeButton';
 import { ThemeEnum } from '../../model/ThemeModel';
 import { useTheme } from '../Theme/ThemeContext';
 import styles from './ThemeButton.module.scss';
 
-const ThemeButton = () => {
+const ThemeButton = ({ size = 'md', disabled }: ThemeButtonProps) => {
 	const { theme, toggleTheme } = useTheme();
 	return (
-		<div className={styles.button} onClick={() => toggleTheme()}>
+		<div
+			className={clsx(styles.button, styles[`s-${size}`], {
+				[styles.disabled]: disabled,
+			})}
+			onClick={() => toggleTheme()}
+		>
 			<i className={'material-symbols-outlined ' + styles.icon}>
 				{theme === ThemeEnum.DARK ? 'dark_mode' : 'wb_sunny'}
 			</i>
