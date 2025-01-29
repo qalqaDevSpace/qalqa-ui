33543bf5e9b200fed886b680cfecf5e47f5095cf
qalqa <andreybas04@gmail.com>
2025-01-29 17:22:53 +0300

    feat: added styles to themeButton

--- src/components-lib/components/ThemeButton/ThemeButton.tsx
+++ src/components-lib/components/ThemeButton/ThemeButton.tsx
@@ -1,11 +1,12 @@
 import { ThemeEnum } from '../../model/ThemeModel';
 import { useTheme } from '../Theme/ThemeContext';
+import styles from './ThemeButton.module.scss';
 
 const ThemeButton = () => {
 	const { theme, toggleTheme } = useTheme();
 	return (
-		<div onClick={() => toggleTheme()}>
-			<i className="material-symbols-outlined">
+		<div className={styles.button} onClick={() => toggleTheme()}>
+			<i className={'material-symbols-outlined ' + styles.icon}>
 				{theme === ThemeEnum.DARK ? 'dark_mode' : 'wb_sunny'}
 			</i>
 		</div>
