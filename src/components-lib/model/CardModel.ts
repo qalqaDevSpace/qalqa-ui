920dbfb600c81ede8018af8fcfd268e8d5bcb44c
TakhirovBizhan <149726252+TakhirovBizhan@users.noreply.github.com>
2025-02-03 23:59:49 +0300

    feat: added new variations to model

--- src/components-lib/model/CardModel.ts
+++ src/components-lib/model/CardModel.ts
@@ -3,7 +3,14 @@ interface ICardModel {
     hoverable?: boolean,
     title?: string,
     subtitle?: string,
-    children?: React.ReactNode;
+    children?: React.ReactNode,
+    classNames?: string,
+    photo?: Photo,
+    photoPosition: PhotoPosition,
+    photoType?: photoType
 }
 
+type PhotoPosition = 'left' | 'right' | 'top' | 'bottom'
+type Photo = { src: string, alt: string }
+type photoType = 'avatar' | 'default';
 export type CardProps = ICardModel;
